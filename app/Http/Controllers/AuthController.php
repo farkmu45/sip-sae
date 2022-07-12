ecef4b4c86651b0a53f92317a28ee97a5b969e05
farkmu45 <farukmicros@outlook.com>
2022-07-12 15:43:31 +0700

    fix: use custom failed message for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+                'nis' => [__('auth.failed')],
             ]);
         }
 
