e5e1b96641f62195368ae7a44d2f9c672611558d
farkmu45 <farukmicros@outlook.com>
2022-07-17 10:20:52 +0700

    refactor: place find button in right side

--- resources/views/filament/pages/nutritions-graph.blade.php
+++ resources/views/filament/pages/nutritions-graph.blade.php
@@ -2,8 +2,10 @@
   <form wire:submit.prevent="submit">
     {{ $this->form }}
 
-    <x-filament::button type="submit" class="mt-3">
-      {{__('text.find')}}
-    </x-filament::button>
+    <div class="flex justify-end mt-3">
+        <x-filament::button type="submit">
+          {{__('text.find')}}
+        </x-filament::button>
+    </div>
   </form>
 </x-filament::page>
