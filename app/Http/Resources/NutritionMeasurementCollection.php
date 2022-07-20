bb910ec299b6e8e043bdf683c981b783d9855d6b
farkmu45 <farukmicros@outlook.com>
2022-07-20 11:57:10 +0700

    fix: wrong cast in measurement

--- app/Http/Resources/NutritionMeasurementCollection.php
+++ app/Http/Resources/NutritionMeasurementCollection.php
@@ -11,10 +11,10 @@ class NutritionMeasurementCollection extends ResourceCollection
         return $this->collection->transform(
             fn ($measurement) => [
                 'id' => $measurement->id,
-                'weight' => $measurement->weight,
-                'height' => $measurement->height_in_meters,
+                'weight' => (int) $measurement->weight,
+                'height' => (float) $measurement->height_in_meters,
                 'status' => $measurement->status->category,
-                'imt' => $measurement->imt,
+                'imt' => (float) $measurement->imt,
                 'created_at' => $measurement->created_at,
                 'updated_at' => $measurement->updated_at,
             ]
