23ace45e902faa82f0cec41fec0396e19557be5d
farkmu45 <farukmicros@outlook.com>
2022-08-07 19:04:27 +0700

    Delete image file when data is updated/deleted

--- app/Models/Narration.php
+++ app/Models/Narration.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Narration extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::updated(function (Narration $model) {
+            if ($model->picture != $model->getOriginal('picture')) {
+                Storage::delete('public/' . $model->getOriginal('picture'));
+            }
+        });
+
+        static::deleted(fn (Narration $model) => Storage::delete('public/' . $model->picture));
+    }
 }
