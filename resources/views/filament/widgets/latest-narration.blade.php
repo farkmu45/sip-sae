e1ced823bad715ffd2a9d5a56c8aba0562d78a95
farkmu45 <farukmicros@outlook.com>
2022-08-07 19:55:40 +0700

    Fix wrong image style

--- resources/views/filament/widgets/latest-narration.blade.php
+++ resources/views/filament/widgets/latest-narration.blade.php
@@ -2,7 +2,7 @@
   <x-filament::card>
     <h2 class="text-xl font-bold tracking-tight md:text-2xl mb-10">Narasi Terbaru</h2>
     <div class="w-full h-80 rounded-md overflow-hidden">
-      <img class="w-full h-full object-cover" src="{{ $narration->picture }}" alt="" srcset="">
+      <img class="w-full h-full object-cover" src="{{ asset('storage/' . $narration->picture) }}" alt="" srcset="">
     </div>
     <h3 class="text-lg pt-5 font-semibold">{{ $narration->title }}</h3>
 
