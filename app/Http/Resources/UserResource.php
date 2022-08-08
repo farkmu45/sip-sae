40e010b5c168e7361e216845325e9d6c63bb6794
farkmu45 <farukmicros@outlook.com>
2022-08-08 16:16:05 +0700

    Add more student's data in login response

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,6 +12,11 @@ class UserResource extends JsonResource
             'name' => $this->student->name,
             'nis' => $this->student_nis,
             'classroom' => $this->student->classroom->name,
+            'date_of_birth' => $this->student->date_of_birth,
+            'gender' => $this->student->gender,
+            'address' => $this->student->address,
+            'school_distance' => $this->student->school_distance,
+            'salary' => $this->student->salary,
             'token' => $this->createToken($request->getClientIp())->plainTextToken,
         ];
     }
