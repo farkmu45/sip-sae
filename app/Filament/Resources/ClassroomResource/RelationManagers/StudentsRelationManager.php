366d4a8bf05effda01695c1d93ffef5015346f43
farkmu45 <farukmicros@outlook.com>
2022-08-12 09:35:31 +0700

    Change section to tabs fro student resource

--- app/Filament/Resources/ClassroomResource/RelationManagers/StudentsRelationManager.php
+++ app/Filament/Resources/ClassroomResource/RelationManagers/StudentsRelationManager.php
@@ -2,9 +2,15 @@
 
 namespace App\Filament\Resources\ClassroomResource\RelationManagers;
 
+use App\Enums\Gender;
+use Carbon\Carbon;
+use Closure;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
@@ -27,61 +33,92 @@ class StudentsRelationManager extends RelationManager
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Section::make(__('text.student_data'))
-                    ->schema(
-                        [
-                            TextInput::make('nis')
+            ->schema(
+                Tabs::make('Heading')->tabs([
+                    Tab::make(__('text.student_data'))
+                        ->schema(
+                            [
+                                TextInput::make('nis')
+                                    ->unique(ignoreRecord: true)
+                                    ->numeric()
+                                    ->length(8)
+                                    ->label(__('text.nis'))
+                                    ->required(),
+                                TextInput::make('name')
+                                    ->label(__('text.name'))
+                                    ->maxLength(45)
+                                    ->required(),
+                                TextInput::make('address')
+                                    ->label(__('text.address'))
+                                    ->maxLength(45)
+                                    ->required(),
+                                DatePicker::make('date_of_birth')
+                                    ->label(__('text.date_of_birth'))
+                                    ->maxDate(now())
+                                    ->rules([
+                                        fn () => function (string $attribute, $value, Closure $fail) {
+                                            $studentAge = Carbon::parse($value)->age;
+                                            $studentDOB = Carbon::parse($value);
+                                            $currentDate = Carbon::parse(now());
+                                            $studentDOBConverted = $studentDOB->year($currentDate->year);
+                                            $monthDifference = $currentDate->diffInMonths($studentDOBConverted);
+
+                                            if (($studentAge >= 5 && $studentAge <= 19)) {
+                                                if ($studentAge == 5 && $monthDifference == 0 || $studentAge == 19 && $monthDifference > 0) {
+                                                    $fail(__('text.invalid_age'));
+                                                }
+                                            } else {
+                                                $fail(__('text.invalid_age'));
+                                            }
+                                        },
+                                    ])
+                                    ->required(),
+                                Select::make('classroom_id')
+                                    ->relationship('classroom', 'name')
+                                    ->label(__('text.classroom'))
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
+                                Radio::make('gender')
+                                    ->label(__('text.gender'))
+                                    ->options([
+                                        Gender::MALE->value => __('text.male'),
+                                        Gender::FEMALE->value => __('text.female'),
+                                    ])
+                                    ->required(),
+                            ]
+                        )
+                        ->columns(2),
+
+                    Tab::make(__('text.parents_data'))
+                        ->schema([
+                            Select::make('job_id')
+                                ->relationship('job', 'name')
+                                ->label(__('text.job'))
+                                ->required(),
+                            TextInput::make('salary')
+                                ->label(__('text.salary'))
+                                ->minValue(1)
                                 ->numeric()
-                                ->label(__('text.nis'))
-                                ->required(),
-                            TextInput::make('name')
-                                ->label(__('text.name'))
-                                ->required(),
-                            TextInput::make('address')
-                                ->label(__('text.address'))
-                                ->required(),
-                            DatePicker::make('date_of_birth')
-                                ->label(__('text.date_of_birth'))
-                                ->maxDate(now())
-                                ->required(),
-                        ]
-                    ),
+                                ->required()
+                                ->mask(fn (TextInput\Mask $mask) => $mask->money('Rp', '.', 0)),
+                            TextInput::make('marital_status_of_parents')
+                                ->label(__('text.marital_status'))
+                                ->maxLength(45)
+                                ->required()
+                                ->columnSpan(2),
+                        ])
+                        ->columns(2),
 
-                Section::make(__('text.parents_data'))
-                    ->schema([
-                        Select::make('job_id')
-                            ->relationship('job', 'name')
-                            ->label(__('text.job'))
-                            ->required()
-                            ->createOptionForm(
-                                [
-                                    TextInput::make('name')
-                                        ->label(__('text.job'))
-                                        ->required(),
-                                ]
-                            ),
-                        TextInput::make('salary')
-                            ->label(__('text.salary'))
-                            ->minValue(0)
-                            ->numeric()
-                            ->required()
-                            ->mask(fn (TextInput\Mask $mask) => $mask->money('Rp', '.', 0)),
-                        TextInput::make('marital_status_of_parents')
-                            ->label(__('text.marital_status'))
-                            ->required()
-                            ->columnSpan(2),
-                    ])
-                    ->columns(2),
-
-                Section::make(__('text.other_information'))
-                    ->schema([
-                        TextInput::make('school_distance')
-                            ->label(__('text.school_distance'))
-                            ->required()
-                            ->numeric(),
-                    ]),
-            ]);
+                    Tab::make(__('text.other_information'))
+                        ->schema([
+                            TextInput::make('school_distance')
+                                ->label(__('text.school_distance'))
+                                ->required()
+                                ->numeric(),
+                        ]),
+                ])
+            );
     }
 
     public static function table(Table $table): Table
--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -11,6 +11,8 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -41,100 +43,92 @@ class StudentResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Section::make(__('text.student_data'))
-                    ->schema(
-                        [
-                            TextInput::make('nis')
-                                ->unique(ignoreRecord: true)
-                                ->numeric()
-                                ->length(8)
-                                ->label(__('text.nis'))
-                                ->required(),
-                            TextInput::make('name')
-                                ->label(__('text.name'))
-                                ->maxLength(45)
-                                ->required(),
-                            TextInput::make('address')
-                                ->label(__('text.address'))
-                                ->maxLength(45)
-                                ->required(),
-                            DatePicker::make('date_of_birth')
-                                ->label(__('text.date_of_birth'))
-                                ->maxDate(now())
-                                ->rules([
-                                    fn () => function (string $attribute, $value, Closure $fail) {
-                                        $studentAge = Carbon::parse($value)->age;
-                                        $studentDOB = Carbon::parse($value);
-                                        $currentDate = Carbon::parse(now());
-                                        $studentDOBConverted = $studentDOB->year($currentDate->year);
-                                        $monthDifference = $currentDate->diffInMonths($studentDOBConverted);
+            ->schema(
+                Tabs::make('Heading')->tabs([
+                    Tab::make(__('text.student_data'))
+                        ->schema(
+                            [
+                                TextInput::make('nis')
+                                    ->unique(ignoreRecord: true)
+                                    ->numeric()
+                                    ->length(8)
+                                    ->label(__('text.nis'))
+                                    ->required(),
+                                TextInput::make('name')
+                                    ->label(__('text.name'))
+                                    ->maxLength(45)
+                                    ->required(),
+                                TextInput::make('address')
+                                    ->label(__('text.address'))
+                                    ->maxLength(45)
+                                    ->required(),
+                                DatePicker::make('date_of_birth')
+                                    ->label(__('text.date_of_birth'))
+                                    ->maxDate(now())
+                                    ->rules([
+                                        fn () => function (string $attribute, $value, Closure $fail) {
+                                            $studentAge = Carbon::parse($value)->age;
+                                            $studentDOB = Carbon::parse($value);
+                                            $currentDate = Carbon::parse(now());
+                                            $studentDOBConverted = $studentDOB->year($currentDate->year);
+                                            $monthDifference = $currentDate->diffInMonths($studentDOBConverted);
 
-                                        if (($studentAge >= 5 && $studentAge <= 19)) {
-                                            if ($studentAge == 5 && $monthDifference == 0 || $studentAge == 19 && $monthDifference > 0) {
+                                            if (($studentAge >= 5 && $studentAge <= 19)) {
+                                                if ($studentAge == 5 && $monthDifference == 0 || $studentAge == 19 && $monthDifference > 0) {
+                                                    $fail(__('text.invalid_age'));
+                                                }
+                                            } else {
                                                 $fail(__('text.invalid_age'));
                                             }
-                                        } else {
-                                            $fail(__('text.invalid_age'));
-                                        }
-                                    },
-                                ])
-                                ->required(),
-                            Select::make('classroom_id')
-                                ->relationship('classroom', 'name')
-                                ->label(__('text.classroom'))
-                                ->searchable()
-                                ->preload()
-                                ->required(),
-                            Radio::make('gender')
-                                ->label(__('text.gender'))
-                                ->options([
-                                    Gender::MALE->value => __('text.male'),
-                                    Gender::FEMALE->value => __('text.female'),
-                                ])
-                                ->required(),
+                                        },
+                                    ])
+                                    ->required(),
+                                Select::make('classroom_id')
+                                    ->relationship('classroom', 'name')
+                                    ->label(__('text.classroom'))
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
+                                Radio::make('gender')
+                                    ->label(__('text.gender'))
+                                    ->options([
+                                        Gender::MALE->value => __('text.male'),
+                                        Gender::FEMALE->value => __('text.female'),
+                                    ])
+                                    ->required(),
+                            ]
+                        )
+                        ->columns(2),
 
-                        ]
-                    )
-                    ->columns(2),
+                    Tab::make(__('text.parents_data'))
+                        ->schema([
+                            Select::make('job_id')
+                                ->relationship('job', 'name')
+                                ->label(__('text.job'))
+                                ->required(),
+                            TextInput::make('salary')
+                                ->label(__('text.salary'))
+                                ->minValue(1)
+                                ->numeric()
+                                ->required()
+                                ->mask(fn (TextInput\Mask $mask) => $mask->money('Rp', '.', 0)),
+                            TextInput::make('marital_status_of_parents')
+                                ->label(__('text.marital_status'))
+                                ->maxLength(45)
+                                ->required()
+                                ->columnSpan(2),
+                        ])
+                        ->columns(2),
 
-                Section::make(__('text.parents_data'))
-                    ->schema([
-                        Select::make('job_id')
-                            ->relationship('job', 'name')
-                            ->label(__('text.job'))
-                            ->searchable()
-                            ->preload()
-                            ->required()
-                            ->createOptionForm(
-                                [
-                                    TextInput::make('name')
-                                        ->label(__('text.job'))
-                                        ->required(),
-                                ]
-                            ),
-                        TextInput::make('salary')
-                            ->label(__('text.salary'))
-                            ->minValue(1)
-                            ->numeric()
-                            ->required()
-                            ->mask(fn (TextInput\Mask $mask) => $mask->money('Rp', '.', 0)),
-                        TextInput::make('marital_status_of_parents')
-                            ->label(__('text.marital_status'))
-                            ->maxLength(45)
-                            ->required()
-                            ->columnSpan(2),
-                    ])
-                    ->columns(2),
-
-                Section::make(__('text.other_information'))
-                    ->schema([
-                        TextInput::make('school_distance')
-                            ->label(__('text.school_distance'))
-                            ->required()
-                            ->numeric(),
-                    ]),
-            ]);
+                    Tab::make(__('text.other_information'))
+                        ->schema([
+                            TextInput::make('school_distance')
+                                ->label(__('text.school_distance'))
+                                ->required()
+                                ->numeric(),
+                        ]),
+                ])
+            );
     }
 
     public static function table(Table $table): Table
